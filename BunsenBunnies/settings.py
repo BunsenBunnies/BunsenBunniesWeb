fe12b286e644c0b9e4c3c5475eb1a179b76e2243
BunsenBunnies <celticdragonstudios@me.com>
2016-07-04 14:25:40 +0100

    update
    update

--- BunsenBunnies/settings.py
+++ BunsenBunnies/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/London'
+TIME_ZONE = 'GB'
 
 USE_I18N = True
 
